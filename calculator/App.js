f9b9ac1a609a130ce983ceb07698c196bee1d4d0
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2020-10-30 07:28:38 +0200

    calculator with sum and substraction

--- calculator/App.js
+++ calculator/App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [firstValue, setFirstValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const sum = () => {
+    const sum = parseInt(firstValue) + parseInt(secondValue)
+    //console.log(sum)
+    setResult(sum)
+  }
+
+  const substraction = () => {
+    const sub = parseInt(firstValue) - parseInt(secondValue)
+    //console.log(sum)
+    setResult(sub)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result: {result}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={firstValue => setFirstValue(firstValue)}
+        value={firstValue}
+        keyboardType={'numeric'}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={secondValue => setSecondValue(secondValue)}
+        value={secondValue}
+        keyboardType={'numeric'}
+      />
+
+      <View style={styles.containerButtons}>
+        <Button
+          onPress={sum}
+          title='+'
+        />
+        <Button
+          onPress={substraction}
+          title='-'
+        />
+      </View>
+
+      <View style={styles.containerBottom}>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +57,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 100,
+  },
+  containerButtons: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+  },
+  containerBottom: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 5,
   },
 });
