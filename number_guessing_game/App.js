f28e06c8d46f1f110a367739713d9c899f325c86
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2020-10-30 20:30:44 +0200

    guess the number

--- number_guessing_game/App.js
+++ number_guessing_game/App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 100 + 1))//number = Math.floor(Math.random() * 100 + 1)
+  const [guess, setGuess] = useState('')
+  const [text, setText] = useState('Guess a number between 1-100')
+  const [amount, setAmount] = useState(0)
+
+  const makeTheGuess = () => {
+    //console.log(guess)
+    //console.log(number)
+    //console.log(amount)
+    if (guess > number) {
+      setText('Your guess is too high')
+      setAmount(amount + 1)
+    } else if (guess < number) {
+      setText('Your guess is too low')
+      setAmount(amount + 1)
+    } else {
+      Alert.alert(`You guessed the number in ${amount} guesses`)
+      setAmount(0)
+      setNumber(Math.floor(Math.random() * 100 + 1))
+      setGuess('')
+      //console.log(number)
+      //console.log(amount)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{text}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={guess => setGuess(guess)}
+        value={guess}
+        keyboardType={'numeric'}
+      />
+      <Button
+        onPress={makeTheGuess}
+        title='MAKE A GUESS'
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +53,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: 50,
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 15,
+  },
+  text: {
+    fontSize: 16,
   },
 });
