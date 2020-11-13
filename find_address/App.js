aea995cc6f84bdb6fad2f4954ede3e8b7051e326
Hanna Ylä-Jussila <hanna.yla-jussila@myy.haaga-helia.fi>
2020-11-13 20:42:20 +0200

    find address with geocoding-api

---
+++ find_address/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { Alert, Button, StyleSheet, TextInput, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
+import Constants from 'expo-constants'
+
+export default function App() {
+  const KEY = Constants.manifest.extra.apiKey
+  const [address, setAddress] = useState()
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221
+  })
+  const url = `http://open.mapquestapi.com/geocoding/v1/address?key=${KEY}&location=${address}`
+
+  const findAddress = () => {
+    //console.log(url)
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data)
+        //console.log(data.results[0].locations[0].latLng)
+
+        setRegion({
+          latitude: Number(data.results[0].locations[0].latLng.lat),
+          longitude: Number(data.results[0].locations[0].latLng.lng),
+          latitudeDelta: 0.0322,
+          longitudeDelta: 0.0221
+        })
+
+        setAddress('')
+      })
+      .catch((error) => {
+        Alert.alert('Error', error)
+      })
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={address => setAddress(address)}
+        value={address}
+        placeholder='Type the address'
+      />
+      <View style={styles.button}>
+        <Button
+          title='SHOW'
+          onPress={findAddress}
+        />
+      </View>
+
+      <MapView
+        style={styles.map}
+
+        region={region}
+        onRegionChangeComplete={region => setRegion(region)}
+      >
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude
+          }}
+        />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  input: {
+    width: '80%',
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'space-around',
+  },
+});
